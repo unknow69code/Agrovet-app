4d675e4236e6eb9c0e3e298bd3d4a3dfb2fe392f
unknow69code <anonymus69007@gmail.com>
2025-05-20 22:49:10 -0500

    change to the API products new 2

--- src/app/api/products/name/[nombre]/stock/route.ts
+++ src/app/api/products/name/[nombre]/stock/route.ts
@@ -1,15 +1,8 @@
-import { NextRequest, NextResponse } from "next/server";
 import { updateStockAndPriceByName } from "@/models/producto";
-import { ResultSetHeader } from 'mysql2';
+import { ResultSetHeader } from "mysql2";
+import { NextResponse } from "next/server";
 
-// Define the type for the params object
-interface ProductParams {
-  nombre: string;
-}
-
-export async function PUT(
-  req: NextRequest, // Use NextRequest instead of Request
-  { params }: { params: ProductParams } // Keep this structure as it's common for App Router
+export async function PUT(req: Request,{ params }: { params: { nombre: string } }
 ) {
   try {
     const { nuevoStock, nuevoPrecio } = await req.json();
@@ -17,6 +10,7 @@ export async function PUT(
     if (!nuevoStock || isNaN(nuevoStock)) {
       return NextResponse.json(
         { error: "Stock inválido" },
+
         { status: 400 }
       );
     }
@@ -24,18 +18,31 @@ export async function PUT(
     if (!nuevoPrecio || isNaN(parseFloat(nuevoPrecio))) {
       return NextResponse.json(
         { error: "Precio inválido" },
+
         { status: 400 }
       );
     }
 
-    const nombreProducto = params.nombre;
+    const nombreProducto = await Promise.resolve(params.nombre);
 
-    const result = await updateStockAndPriceByName(nombreProducto, nuevoStock, parseFloat(nuevoPrecio)) as ResultSetHeader;
+    const result = (await updateStockAndPriceByName(
+      nombreProducto,
+      nuevoStock,
+      parseFloat(nuevoPrecio)
+    )) as ResultSetHeader;
 
     if (result?.affectedRows > 0) {
-      return NextResponse.json({ message: "Stock y precio actualizados", result });
+      return NextResponse.json({
+        message: "Stock y precio actualizados",
+        result,
+      });
     } else {
-      return NextResponse.json({ error: `No se encontró ningún producto con el nombre '${nombreProducto}'` }, { status: 404 });
+      return NextResponse.json(
+        {
+          error: `No se encontró ningún producto con el nombre '${nombreProducto}'`,
+        },
+        { status: 404 }
+      );
     }
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
